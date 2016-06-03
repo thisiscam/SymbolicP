f638551381915ae7d4889b74bf2791bd7a8ef2e4
jq.yang@berkeley.edu <jq.yang@berkeley.edu>
2016-06-02 20:33:07 -0700

    convert indentation to spaces

--- playground/pingpong/MachineClient.cs
+++ playground/pingpong/MachineClient.cs
@@ -4,44 +4,44 @@ using System;
 using System.Diagnostics;
 
 class MachineClient : PMachine {
-	/* P local vars */
-	PMachine server;
+    /* P local vars */
+    PMachine server;
 
-	public MachineClient() {
-		state = 0;
-		InitEntry();
-	}
-	public void InitEntry() {
-		this.server = Inst.Set_Prop<PMachine>(this.server, new MachineServer());
-		Scheduler.machines.Add(server);
-		Init_RaiseEvent(SUCCESS); return;
-	}
-	public void SendPingEntry() {
-		sendMsg(this.server, PING, new object[]{this});
-		SendPing_RaiseEvent(SUCCESS); return;
-	}
-	public void Init_RaiseEvent(int e) {		
-		int pc = 0;
-		while(true) {
-		switch(pc) {
-			case  0:	if(e != SUCCESS) {pc = 4; break;}						
-						this.state = Inst.Set_Prop<int>(this.state, MachineClient_STATE_SendPing);								
-						this.SendPingEntry();						
-						pc = -1; break;													
-			CASE(4):	throw new SystemException("Unhandled event");	
-			CASE(-1):	return;
-		}}
-	}
-	public void SendPing_RaiseEvent(int e) {
-		int pc = 0;
-		while(true) {
-		switch(pc) {
-			case  0:	if(e != SUCCESS) {pc = 3; break;}						
-						this.state = Inst.Set_Prop<int>(this.state, MachineClient_STATE_WaitPong);																				
-						pc = 4; break;									
-			CASE(3):	throw new SystemException("Unhandled event");	
-			CASE(4):	return;
-		}}
-	}
+    public MachineClient() {
+        state = 0;
+        InitEntry();
+    }
+    public void InitEntry() {
+        this.server = Inst.Set_Prop<PMachine>(this.server, new MachineServer());
+        Scheduler.machines.Add(server);
+        Init_RaiseEvent(SUCCESS); return;
+    }
+    public void SendPingEntry() {
+        sendMsg(this.server, PING, new object[]{this});
+        SendPing_RaiseEvent(SUCCESS); return;
+    }
+    public void Init_RaiseEvent(int e) {        
+        int pc = 0;
+        while(true) {
+        switch(pc) {
+            case  0:    if(e != SUCCESS) {pc = 4; break;}                       
+                        this.state = Inst.Set_Prop<int>(this.state, MachineClient_STATE_SendPing);                              
+                        this.SendPingEntry();                       
+                        pc = -1; break;                                                 
+            CASE(4):    throw new SystemException("Unhandled event");   
+            CASE(-1):   return;
+        }}
+    }
+    public void SendPing_RaiseEvent(int e) {
+        int pc = 0;
+        while(true) {
+        switch(pc) {
+            case  0:    if(e != SUCCESS) {pc = 3; break;}                       
+                        this.state = Inst.Set_Prop<int>(this.state, MachineClient_STATE_WaitPong);                                                                              
+                        pc = 4; break;                                  
+            CASE(3):    throw new SystemException("Unhandled event");   
+            CASE(4):    return;
+        }}
+    }
 
 }
--- playground/pingpong/PMachine.cs
+++ playground/pingpong/PMachine.cs
@@ -2,13 +2,13 @@ using System;
 using System.Collections.Generic;
 
 class PMachine {
-	public int state;
-	public List<int> events = new List<int>();
-	public List<object[]> payloads = new List<object[]>();
+    public int state;
+    public List<int> events = new List<int>();
+    public List<object[]> payloads = new List<object[]>();
 
-	public void sendMsg(PMachine other, int e, object[] payload) {
-		Console.WriteLine(this.ToString() + " send event " + e.ToString() + " to " + other.ToString());
-		other.events.Add(e);
-		other.payloads.Add(payload);
-	}
+    public void sendMsg(PMachine other, int e, object[] payload) {
+        Console.WriteLine(this.ToString() + " send event " + e.ToString() + " to " + other.ToString());
+        other.events.Add(e);
+        other.payloads.Add(payload);
+    }
 }
