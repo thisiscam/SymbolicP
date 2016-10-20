e5545455ab1b527e5e41b329ebd451a45aa9a735
jq.yang@berkeley.edu <jq.yang@berkeley.edu>
2016-10-20 00:06:32 -0700

    update multise transformer

--- translator/translators/multise_csharp_translator/valuesummary_translator.py
+++ translator/translators/multise_csharp_translator/valuesummary_translator.py
@@ -1,15 +1,16 @@
 import subprocess, os
 
 transformer_proj_path = os.path.realpath(os.path.join(os.path.dirname(__file__), "MultiSETransformer"))
-transformer_exe = os.path.join(transformer_proj_path, "MultiSETransformer.exe")
-transformer_lib_path = os.path.join(transformer_proj_path, "Libs")
+for root, dirs, files in os.walk(os.path.join(transformer_proj_path, "bin"), topdown=False):
+    for name in files:
+        if name == "MultiSETransformer.exe":
+        	transformer_exe = os.path.join(root, name)
+        	break
 
 if os.name == "nt":
 	transformer_cmd = [transformer_exe]
-	ld_library_path_var = "PATH"
 else:
 	transformer_cmd = ["mono", transformer_exe]
-	ld_library_path_var = "MONO_PATH"
 
 def valuesummary_transform(include_files, output_path, transform_files, no_copy_srcs=None):
 	if not isinstance(include_files, str):
@@ -17,10 +18,5 @@ def valuesummary_transform(include_files, output_path, transform_files, no_copy_
 	cmd = transformer_cmd + [include_files, output_path, ",".join(transform_files)]
 	if no_copy_srcs:
 		cmd += [",".join(no_copy_srcs)]
-	env = os.environ.copy()
-	if ld_library_path_var in env:
-		env[ld_library_path_var] += os.pathsep + transformer_lib_path
-	else:
-		env[ld_library_path_var] = transformer_lib_path
 	# print " ".join(cmd)
-	subprocess.check_call(cmd, env=env)
+	subprocess.check_call(cmd)
